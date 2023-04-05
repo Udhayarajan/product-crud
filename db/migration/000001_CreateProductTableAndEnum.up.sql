a49329329d5e709ab067ada6fb109237a8774c88
udhayarajan <mudhayarajan.2013@gmail.com>
2023-04-05 20:48:44 +0530

    style: formatted migration file

--- db/migration/000001_CreateProductTableAndEnum.up.sql
+++ db/migration/000001_CreateProductTableAndEnum.up.sql
@@ -7,16 +7,16 @@ CREATE TYPE product_type AS ENUM (
     'TypeB',
     'TypeC',
     'TypeD'
-    );
+);
 
 
 CREATE TABLE IF NOT EXISTS "products"(
-"id" UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-"name" VARCHAR(100) PRIMARY KEY NOT NULL CHECK(LENGTH(name)>5),
-"quantity" INT,
-"type" product_type NOT NULL ,
-"price" NUMERIC(10, 2) NOT NULL CHECK(price>=0) DEFAULT 0.00,
-"description" varchar(500)
+    "id" UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(100) PRIMARY KEY NOT NULL CHECK(LENGTH(name)>5),
+    "quantity" INT,
+    "type" product_type NOT NULL ,
+    "price" NUMERIC(10, 2) NOT NULL CHECK(price>=0) DEFAULT 0.00,
+    "description" varchar(500)
 );
 
 COMMIT;
